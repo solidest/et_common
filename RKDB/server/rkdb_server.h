fba1eed0a2bf7ebd34bdc9ea2d493cd0f4cdf5e7
solidest <solidest@163.com>
2018-11-28 20:25:33 -0800

    add rkcb2

---
+++ RKDB/server/rkdb_server.h
@@ -0,0 +1,15 @@
+/*
+** rkdb_server.h for et_common in /home/byx/et/et_common/RKDB/server
+**
+** Made by header_template
+** Login   <>
+**
+** Started on  Wed Nov 28 8:24:43 PM 2018 header_template
+** Last update Thu Nov 28 8:25:25 PM 2018 header_template
+*/
+
+#ifndef RKDB_SERVER_H_
+# define RKDB_SERVER_H_
+
+
+#endif /* !RKDB_SERVER_H_ */
